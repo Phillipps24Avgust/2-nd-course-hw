eeeb2edd27fd5bb794ef0449b93ff83545f7a6a5
Phillipps24Avgust <Tarelka1999B@yandex.ru>
2023-03-28 00:30:37 +1000

    Mobile version and the main js assignment

---
+++ hw3.js
@@ -0,0 +1,70 @@
+// задание 1
+
+// let password = 'пароль';
+
+// let entry = String(prompt('Введите пароль'));
+
+// let respond = entry !== password ? 'пароль введён неверно' : 'пароль введён верно';
+// alert(respond);
+
+// задание 2
+
+// let c = 11;
+// c >= 0 && c < 10 ? console.log('Верно') : console.log('Неверно');
+
+// c = 0;
+// c >= 0 && c < 10 ? console.log('Верно') : console.log('Неверно');
+
+// c = 10;
+// c >= 0 && c < 10 ? console.log('Верно') : console.log('Неверно');
+
+// c = -3;
+// c >= 0 && c < 10 ? console.log('Верно') : console.log('Неверно');
+
+// c = 2;
+// c >= 0 && c < 10 ? console.log('Верно') : console.log('Неверно');
+
+// задание 3
+
+// let d, e;
+// d = 15, e = 105;
+
+// d > 100 || e > 100 ? console.log('Верно') : console.log('Неверно');
+
+// задание 4
+
+// let a = '2';
+// let b = '3';
+
+// a = parseInt('2', 10);
+// b = parseInt('3', 10);
+
+// alert(a + b);
+
+// задание 5
+
+// let monthNumber = Number(prompt('Введите номер месяца и узнайте, к какому сезону он относится'));
+
+// if (monthNumber === 12 || monthNumber <= 2 && monthNumber > 0) {
+//     alert('Это зима!');
+// } else if (monthNumber >= 3 && monthNumber <= 5) {
+//     alert('Это весна!');
+// } else if (monthNumber >= 6 && monthNumber <= 8) {
+//     alert('Это лето!');
+// } else if (monthNumber >= 9 && monthNumber <= 11) {
+//     alert('Это осень!');
+// } else if (monthNumber === 0) {
+//     alert('В году нет месяца 0!');
+// } else if (monthNumber < 0) {
+//     alert('В году нет отрицательных месяцев!');
+// } else {
+//     alert('В году только 12 месяцев, которые также не могут быть дробными! При введении названия месяца вместо его номера вы также увидите эту ошибку!')
+// }
+
+
+
+
+
+
+
+
