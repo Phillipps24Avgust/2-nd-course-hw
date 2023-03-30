ff7aaa5525f9e6a689374f774054c96d5b248a5a
Phillipps24Avgust <Tarelka1999B@yandex.ru>
2023-03-30 22:15:16 +1000

    Done hw4

---
+++ hw4.js
@@ -0,0 +1,57 @@
+// задание 1
+
+// let i = 0;
+
+// while (i < 2) {
+//     console.log("Привет!");
+//     i++;
+// }
+
+// задание 2
+
+// for (let i = 1; i < 6; i++) {
+//     console.log(i);
+// }
+
+// задание 3
+
+// for (let i = 7; i < 23; i++) {
+//     console.log(i);
+// }
+
+// задание 4
+
+// const obj = {
+//     "Коля": "200",
+//     "Вася": "300",
+//     "Петя": "400",
+// }
+
+// for (personName in obj) {
+//     console.log(`${personName} – зарплата ${obj[personName]}`);
+// }
+
+// задание 5
+
+// let num = 0;
+
+// for (let n = 1000; n > 50; num++) {
+//     n = n/2;
+//     console.log(n);
+// }
+
+// console.log(`Операция задействовала ${num} операций`);
+
+// задание 6
+
+// let friday = 0;
+
+// for (let month = 0; friday < 4; friday++) {
+//     month += 7;
+//     console.log(`Сегодня пятница, ${month}-ое число. Необходимо подготовить отчёт`);
+// }
+
+
+
+
+
