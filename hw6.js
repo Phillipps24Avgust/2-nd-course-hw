0de6be691e1d2446c09978860533e857c975bb4c
Phillipps24Avgust <Tarelka1999B@yandex.ru>
2023-04-06 22:08:54 +1000

    Done 1 to 8 tasks

---
+++ hw6.js
@@ -0,0 +1,80 @@
+// задание 1
+
+// const arr = [1, 5, 4, 10, 0, 3];
+
+// for (let i = 0; i < 4; i++) {
+//     console.log(arr[i]);
+// }
+
+// задание 2
+
+// const arr = [1, 5, 4, 10, 0, 3];
+
+// console.log(arr.indexOf(4));
+
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+// задание 3 
+
+// const arr = [1, 3, 5, 10, 20];
+
+// console.log(arr.join(' '));
+
+// задание 4
+
+// const arr = [];
+
+// for (let i = 0; i < 3; i++) {
+//     arr[i] = []; 
+
+//     for (let j = 0; j < 3; j++) {
+//         arr[i].push(j + 1);
+//     }
+// } 
+ 
+// console.log(arr);
+
+// задание 5
+
+// const arr = [1, 1, 1];
+
+// for (let i = 0; i < 3; i++) {
+//     arr.push(2);
+// }
+
+// console.log(arr);
+
+// задание 6
+
+// let arr = [9, 8, 7, 'a', 6, 5];
+
+// arr = arr.sort();
+// arr.pop();
+
+// console.log(arr);
+
+// задание 7
+
+// const arr = [9, 8, 7, 6, 5];
+
+// let typedNumber = Number(prompt("Введите любое целое число от 1 до 10"));
+
+// if (arr.includes(typedNumber)) {
+//     console.log("Да, в массиве есть такое число!");
+// } else {
+//     console.log("Увы, такого числа в массиве нет!");
+// }
+
+// задание 8
+
+// let arr = ['abcdef'];
+// let arrSplited = String(arr).split('');
+
+// arrSplited = arrSplited.reverse();
+// arrSplited = arrSplited.join("");
+// console.log(arrSplited);
+
+
+
+
+
