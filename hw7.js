04f280785f5e9acd0b333e2240d1f78daa00b6c4
Phillipps24Avgust <Tarelka1999B@yandex.ru>
2023-04-11 22:47:34 +1000

    Done 7 out of 11 tasks

---
+++ hw7.js
@@ -0,0 +1,76 @@
+// задание 1
+
+// let lowerCaseString = 'строка в нижнем регистре';
+// console.log(lowerCaseString.toLocaleUpperCase());
+
+// задание 2
+
+// const someAnimals = (['Кошка', 'Кит', 'Комар', 'Носорог']);
+// const search = 'Ко';
+
+// someAnimals.forEach((someAnimal) => {
+// 	if (someAnimal.toLowerCase().includes(search.toLowerCase())) {
+// 		console.log(someAnimal);
+// 	}
+// });
+
+// задание 3
+
+// let number = 32.58884;
+
+// console.log(Math.floor(number));
+// console.log(Math.ceil(number));
+// console.log(number = parseFloat(number.toFixed(1)));
+
+// задание 4
+
+// const numbers = [52, 53, 49, 77, 21, 32];
+
+// const getNumbers = () => {
+//     minValue = Math.min(...numbers);
+//     maxValue = Math.max(...numbers);
+//     return(`Минимальное значение равно ${minValue}, а максимальное – ${maxValue}`);
+// }
+
+// console.log(getNumbers(numbers));
+
+// задание 5
+
+// const getRandomNumber = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// console.log(getRandomNumber(0, 100));
+
+// задание 6
+
+// const createRandomArray = (a) => {
+
+//     let randomNumbersArray = [];
+//     let min = 0;
+
+//     for (let i = 0; i < Math.floor(a / 2); i++) {
+//         randomNumbersArray.push(Math.floor(Math.random() * (a - min + 1)) + min);
+//     }
+
+//     return randomNumbersArray;
+// }
+
+// console.log(createRandomArray(10));
+
+// задание 7
+
+// const getRandomNumber = (min, max) => {
+//     return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// console.log(getRandomNumber(2, 8));
+
+
+
+
+
+
+
+
+
